b6d8fea4cd1371ad946167bb3606f5e5fb1f72ca
SarahRothwell <sarahhrothwell@gmail.com>
2019-07-16 15:07:36 -0400

    first commit with user model

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+//connect database
+connectDB();
+
+//Initialize Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running")); //bodyParser
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
